7a548c98ddd3ee965d80463fe02e8d37e4d5464a
uvvinay <u.v.vinaykumr@gmail.com>
2026-01-14 21:10:52 +0530

    nopcommerce automation scripts first commit

--- POM/AddCustomer.py
+++ POM/AddCustomer.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.select import Select
 
 
 class AddCustomer:
-    link_customer_main_menu_xpath ='//body/div[3]/aside[1]/div[1]/div[4]/div[1]/div[1]/nav[1]/ul[1]/li[4]/a[1]'
+    #link_customer_main_menu_xpath ='//body/div[3]/aside[1]/div[1]/div[4]/div[1]/div[1]/nav[1]/ul[1]/li[4]/a[1]'
     link_customer_sub_menu_xpath = '/html/body/div[3]/aside/div/div[4]/div/div/nav/ul/li[4]/ul/li[1]/a/p'
     btn_add_customer_xpath = "//a[@class='btn btn-primary']"
     txtbox_email_id = "Email"
@@ -45,7 +45,7 @@ class AddCustomer:
         #self.driver.find_element(by=By.XPATH,value=self.link_customer_main_menu_xpath).send_keys()
 
     def customer_sub_menu(self):
-        self.driver.find_element(by=By.XPATH, value=self.link_customer_sub_menu_xpath).click()
+        self.driver.find_element(by=By.XPATH, value=self.D).click()
 
     def add_customer(self):
         self.driver.find_element(by=By.XPATH, value=self.btn_add_customer_xpath).click()
