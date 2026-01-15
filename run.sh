3f191a7a0c6626ae17f496316dc0d681e66b293e
vinay <u.v.vinaykumar@gmail.com>
2026-01-15 22:52:04 +0530

    Update pytest command to use virtual environment

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pytest -v -s --html=./Reports/report.html Test_cases/
+./venv/bin/pytest -v -s --html=./Reports/report.html Test_cases/
 #REM pytest -v -s -m "sanity" --html=./Reports/report.html Test_cases/ --browser chrome
 #REM pytest -v -s -m "regression" --html=./Reports/report.html Test_cases/ --browser chrome
 #REM pytest -v -s --html=./Reports/report.html Test_cases/ --browser firefox
