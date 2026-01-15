6441031d0cfd80db4e8fe55cfc468002f4332e82
vinay <u.v.vinaykumar@gmail.com>
2026-01-15 22:49:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pytest -v -s --html=./Reports/report.html Test_cases/
 #REM pytest -v -s -m "sanity" --html=./Reports/report.html Test_cases/ --browser chrome
 #REM pytest -v -s -m "regression" --html=./Reports/report.html Test_cases/ --browser chrome
