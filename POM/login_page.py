469f48f0c91d09d88796d89a21c2802be0122a14
vinay <u.v.vinaykumar@gmail.com>
2026-01-16 19:38:11 +0530

    Update login_page.py

--- POM/login_page.py
+++ POM/login_page.py
@@ -2,21 +2,21 @@ from selenium.webdriver.common.by import By
 
 
 class login:
-    text_box_username_name="Email"
-    text_box_password_id="Password"
-    button_login_xpath= "//button[@type='submit']"
-    logout_linktext= "/logout"
+    #text_box_username_name="Email"
+    #text_box_password_id="Password"
+    #button_login_xpath= "//button[@type='submit']"
+    #logout_linktext= "/logout"
 
     def __init__(self,driver):
         self.driver = driver
     def setUsername(self,username):
-        self.driver.find_element(by=By.NAME, value= self.text_box_username_name).clear()
-        self.driver.find_element(By.NAME, value=self.text_box_username_name).send_keys(username)
+        self.driver.find_element(By.NAME, "Email").clear()
+        self.driver.find_element(By.NAME, "Email").send_keys(username)
     def setpassword(self,password):
-        self.driver.find_element(by=By.NAME, value=self.text_box_password_id).clear()
-        self.driver.find_element(by=By.ID, value=self.text_box_password_id).send_keys(password)
+        self.driver.find_element(by=By.NAME, "Password").clear()
+        self.driver.find_element(by=By.ID, "Password").send_keys(password)
     def clickLoginButton(self):
-        self.driver.find_element(by=By.XPATH, value=self.button_login_xpath).click()
+        self.driver.find_element(by=By.XPATH, "//button[@type='submit']").click()
 
     def clickLogoutButton(self):
-        self.driver.find_element(by=By.LINK_TEXT, value=self.logout_linktext).click()
+        self.driver.find_element(by=By.LINK_TEXT, "/logout").click()
